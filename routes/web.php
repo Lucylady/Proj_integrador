3f8532afc76b9da5fc1dc9154b15bb761ae0e455
Lucylady Chrystina Tavares do Nascimento <lucylady.c@academico.ifrn.edu.br>
2019-04-11 16:18:55 -0300

    colocando um alerta

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/selecionados', function () {
+    return view('selecionados');
 });
