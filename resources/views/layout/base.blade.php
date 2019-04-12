e11d6ff9b91fa0759e69065e8aab6e8aba567302
brunogomesifrn <brunogomesifrn@gmail.com>
2019-04-11 21:29:06 -0300

    Correcoes em index

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -38,7 +38,7 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-info" >
-  <a class="navbar-brand" href="index"><strong>Início</strong></a>
+  <a class="navbar-brand" href="/"><strong>Início</strong></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
